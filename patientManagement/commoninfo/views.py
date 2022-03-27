19198ff1288fafb8f546df59cd58ff2fdd9836e9
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2022-03-27 12:26:14 +0100

    Add basic views and urls to app

--- patientManagement/commoninfo/views.py
+++ patientManagement/commoninfo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Index<h1>")
+
+def add(request):
+    return HttpResponse("<h1>Add</h1>")
+
+def fetch(request):
+    return HttpResponse("<h1>Fetch</h1>")
